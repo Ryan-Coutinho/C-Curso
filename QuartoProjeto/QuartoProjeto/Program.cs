dfbe0c48b06322c2a8504f1557b23290f16021ef
Ryan Coutinho dos Santos <111476443+Ryan-Coutinho@users.noreply.github.com>
2025-09-15 20:39:07 -0400

    Add files via upload

---
+++ QuartoProjeto/QuartoProjeto/Program.cs
@@ -0,0 +1,8 @@
+﻿int funcionario = int.Parse(Console.ReadLine()!);
+int horaTrabalhada = int.Parse(Console.ReadLine()!);
+double valorHora = double.Parse(Console.ReadLine()!);
+
+double salario = horaTrabalhada * valorHora;
+
+Console.WriteLine($"NUMBER = {funcionario}");
+Console.WriteLine($"SALARY = U$ {salario:f2}");
