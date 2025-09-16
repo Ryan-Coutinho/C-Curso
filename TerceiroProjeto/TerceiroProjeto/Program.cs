e93d6389c6abfe07fa079879687cfc700822efe9
Ryan Coutinho dos Santos <111476443+Ryan-Coutinho@users.noreply.github.com>
2025-09-15 20:38:40 -0400

    Add files via upload

---
+++ TerceiroProjeto/TerceiroProjeto/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int d = int.Parse(Console.ReadLine()!);
+
+int DIFERENCA = (a * b - c * d);
+
+Console.Write(DIFERENCA);
