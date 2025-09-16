6a898b34bb1a328ee3df4388caf485dad5b53b3e
Ryan Coutinho dos Santos <111476443+Ryan-Coutinho@users.noreply.github.com>
2025-09-15 20:42:13 -0400

    Add files via upload

---
+++ CondicionalQuatro/CondicionalQuatro/Program.cs
@@ -0,0 +1,14 @@
+﻿int horaInicial = int.Parse(Console.ReadLine()!);
+int horaFinal = int.Parse(Console.ReadLine()!);
+
+int duracao;
+if (horaInicial < horaFinal)
+{
+    duracao = horaFinal - horaInicial;
+}
+else
+{
+    duracao = 24 - horaInicial + horaFinal;
+}
+
+Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
