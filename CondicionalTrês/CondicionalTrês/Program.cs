1d0beb6845f24479d6e4fb7dd65d07d6e023442a
Ryan Coutinho dos Santos <111476443+Ryan-Coutinho@users.noreply.github.com>
2025-09-15 20:41:50 -0400

    Add files via upload

---
+++ CondicionalTrês/CondicionalTrês/Program.cs
@@ -0,0 +1,15 @@
+﻿int x = int.Parse(Console.ReadLine()!);
+int y = int.Parse(Console.ReadLine()!);
+
+if (x % y == 0)
+{
+    Console.WriteLine($"{x} é multiplo de {y}");
+}
+else if (y % x == 0)
+{
+    Console.WriteLine($"{y} é multiplo de {x}");
+}
+else
+{
+    Console.WriteLine("Não são multiplos");
+}
